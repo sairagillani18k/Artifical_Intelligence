dd4a3188abd1ce194444143756667ce1ae1b1f0c
Saira Gillani <58274863+sairagillani18k@users.noreply.github.com>
2022-11-05 13:09:11 -0400

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+from playsound import playsound
+
+audio = 'speech.mp3'
+language = 'en'
+#speech = gTTS(text="Hey Guys! Thanks for watching", lang=language)
+speech = gTTS(text="Every good deep is charity. The Prophet Muhammad (Peace Be Upon Him)", lang=language)
+
+speech.save(audio)
+playsound("speech.mp3")
