3544224b9b4323b21a86136b75f39318e4764283
Saira Gillani <58274863+sairagillani18k@users.noreply.github.com>
2022-11-05 13:10:26 -0400

    Update text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -4,7 +4,7 @@ from playsound import playsound
 audio = 'speech.mp3'
 language = 'en'
 #speech = gTTS(text="Hey Guys! Thanks for watching", lang=language)
-speech = gTTS(text="Every good deep is charity. The Prophet Muhammad (Peace Be Upon Him)", lang=language)
+speech = gTTS(text="Every good deed is charity. The Prophet Muhammad (Peace Be Upon Him)", lang=language)
 
 speech.save(audio)
 playsound("speech.mp3")
